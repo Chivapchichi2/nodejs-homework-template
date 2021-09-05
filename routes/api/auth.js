d6bda6d3594901640eec85134cabff3d30ce15ff
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-09-05 17:50:55 +0300

    signup, login

---
+++ routes/api/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const { userJoiSchema } = require("../../models/user");
+const { validation, tryCatchWrapper, authenticate } = require("../../middleware");
+const { auth: ctrl } = require("../../controllers");
+
+const router = express.Router();
+
+const userValidationMiddleware = validation(userJoiSchema);
+
+router.post("/signup", userValidationMiddleware, tryCatchWrapper(ctrl.signup));
+
+router.post("/login", userValidationMiddleware, tryCatchWrapper(ctrl.login));
+
+// router.get("/logout", tryCatchWrapper(authenticate), tryCatchWrapper(ctrl.logout));
+
+module.exports = router;
